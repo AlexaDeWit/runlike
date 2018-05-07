081e0a43c1192be0c80964e7f91b136378f16d7c
Alexandra DeWit <alexa.dewit@gmail.com>
2018-05-07 08:03:58 +0200

    Cleanup and formatting of export script

--- tiled_scripts/haskellExport.py
+++ tiled_scripts/haskellExport.py
@@ -1,6 +1,11 @@
 from tiled import *
+from string import Template
+
 
 class HaskellExport(Plugin):
+
+    tileItemTemplate = Template('${prefix}${body}')
+
     @classmethod
     def nameFilter(cls):
         return "Haskell maps (*.hs)"
@@ -8,22 +13,31 @@ class HaskellExport(Plugin):
     @classmethod
     def shortName(cls):
         return "Haskell Exporter"
-        
+
     @classmethod
     def backgroundLayerComponent(cls, tile):
         if tile is None:
             return "Grass"
         idNum = tile.id()
-        indices = ["Grass", "Snow", "Dirt", "Sand", "Water", "Ice", "Rocks", "Mud"]
-        return indices [idNum] if idNum < len(idices) else "Grass"
-        
+        indices = [
+                "Grass", "Snow", "Dirt", "Sand", "Water", "Ice", "Rocks", "Mud"
+            ]
+        return indices[idNum] if idNum < len(indices) else "Grass"
+
     @classmethod
     def foregroundLayerComponent(cls, tile):
         if tile is None:
-            return "Nothing" 
+            return "Nothing"
         idNum = tile.id()
-        indices = ["Cedar", "Deciduous", "Bush", "Flowers", "Plants", "Weeds", "Cactus", "Rocks"]
-        return '( Just ${indices[idNum]} )' if idNum < len(indices) else "Nothing"
+        indices = [
+                "Cedar", "Deciduous", "Bush", "Flowers",
+                "Plants", "Weeds", "Cactus", "Rocks"
+            ]
+
+        if idNum < len(indices):
+            return '( Just ${indices[idNum]} )'
+        else:
+            return "Nothing"
 
     @classmethod
     def movementLayerComponent(cls, tile):
@@ -44,37 +58,41 @@ class HaskellExport(Plugin):
         idNum = tile.id()
         indices = []
         return indices[idNum] if idNum < len(indices) else "[]"
-        
+
     @classmethod
     def tilestackToHaskellTile(cls, tileMap, x, y):
-      return ''
-      
+        return ''
+
     @classmethod
     def hasAllNeededTileLayers(cls, tileMap, requiredLayers):
         neededLayerNumbers = range(0, requiredLayers)
         if all(isTileLayerAt(tileMap, index) for index in neededLayerNumbers):
             backgroundLayer = tileLayerAt(tileMap, requiredLayers)
-            for layerNumber in range(1,5):
+            for layerNumber in range(1, 5):
                 layer = tileLayerAt(tileMap, layerNumber)
-                if(layer.width() != backgroundLayer.width() or layer.height() != backgroundLayer.height()):
-                    return False
+                if(layer.width() != backgroundLayer.width()
+                        or layer.height() != backgroundLayer.height()):
+                            return False
             return True
         return False
-        
-        
+
     @classmethod
     def write(cls, tileMap, fileName):
         requiredLayers = 5
         with open(fileName, 'w') as fileHandle:
-            if hasAllNeededTileLayer(cls, tileMap, requiredLayers):
+            if cls.hasAllNeededTileLayer(cls, tileMap, requiredLayers):
                 backgroundTileLayer = tileLayerAt(tileMap, 0)
                 textLines = []
                 for y in range(backgroundTileLayer.height()):
                     for x in range(backgroundTileLayer.width()):
                         prefix = '    [ ' if x == 0 else '    , '
-                        textLines.append('${prefix}${cls.tilestackToHaskellTile(cls, tileMap, x, y)}')
+                        textLines.append(
+                            cls.tileItemTemplate.substitute(
+                                prefix=prefix,
+                                body=cls.tilestackToHaskellTile(
+                                    cls, tileMap, x, y
+                                )
+                            ))
                     textLines.append('    ]')
                 for line in textLines:
                     print >>fileHandle, line
-
-
