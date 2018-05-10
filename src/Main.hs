3030a7515873d2b39fad82f6a17715130797895a
Alexandra DeWit <alexa.dewit@gmail.com>
2018-05-10 10:40:11 +0200

    Code organization and normalization

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,17 @@
 module Main where
 
-import           Linear.V2       (V2 (V2))
-
 import           Helm
 import           Helm.Sub
 import           Helm.Color
 import           Helm.Engine.SDL (SDLEngine)
 import           Helm.Graphics2D
-import           Types.GameMap
-import           Maps.TestTown as TestTown
+import           Model.Model     (Model (Model), initial)
 
 import qualified Helm.Cmd        as Cmd
 import qualified Helm.Engine.SDL as SDL
 
 data Action = Idle
-data Model = Model (V2 Double) GameMap
 
-initial :: (Model, Cmd SDLEngine Action)
-initial = (Model (V2 0 0) TestTown.map, Cmd.none)
 
 update :: Model -> Action -> (Model, Cmd SDLEngine Action)
 update model Idle             = (model, Cmd.none)
@@ -26,7 +20,7 @@ subscriptions :: Sub SDLEngine Action
 subscriptions = Helm.Sub.none
 
 view :: Model -> Graphics SDLEngine
-view (Model pos _) = Graphics2D $ collage [move pos $ filled (rgb 1 0 0) $ square 10]
+view (Model _) = Graphics2D $ collage [filled (rgb 1 0 0) $ square 10]
 
 main :: IO ()
 main = do
---
+++ src/Model/Model.hs
@@ -0,0 +1,15 @@
+module Model.Model where
+
+
+import           Helm
+import           Types.GameMap
+import           Maps.TestTown as TestTown
+import           Helm.Engine.SDL (SDLEngine)
+import qualified Helm.Cmd        as Cmd
+
+data Model = Model GameMap
+
+
+
+initial :: (Model, Cmd SDLEngine a)
+initial = (Model TestTown.map, Cmd.none)
