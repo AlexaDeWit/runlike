6c7b60c433eb0017004e150c27899d97ee56fc91
Alexandra DeWit <alexa.dewit@gmail.com>
2018-05-13 00:51:43 +0200

    We can render the BG map!

--- src/View.hs
+++ src/View.hs
@@ -4,8 +4,11 @@ module View where
 import           Helm
 import           Model
 import           Helm.Graphics2D
-import           Types.Tile        (Background(..), Foreground(..))
+import           Types.GameMap
+import           Types.Tile        (Background(..), Foreground(..), Tile(..))
 import           Types.Tileset     (ImageScaleRate(..))
+import           Data.Array        (assocs)
+import           Linear.V2         (V2(V2))
 
 data PreparedTilesets e
   = PreparedTilesets
@@ -20,7 +23,14 @@ view :: PreparedTilesets engine -> Model -> Graphics engine
 view tilesets model = Graphics2D $ collage [toForm $ bgLayer tilesets model, toForm $ fgLayer tilesets model]
 
 bgLayer :: PreparedTilesets e -> Model -> Collage e
-bgLayer tilesets _ = collage [backgroundTile tilesets Water]
+bgLayer tilesets model = collage positionedTiles where
+  mapTiles = fmap (backgroundTile tilesets . tileBackground) (tiles $ currentMap model)
+  positionedTiles = fmap (mapOffset $ tileScaleRate tilesets) (assocs mapTiles)
 
 fgLayer :: PreparedTilesets e -> Model -> Collage e
 fgLayer tilesets _ = collage [foregroundTile tilesets Rocks]
+
+
+mapOffset :: ImageScaleRate -> ((Int, Int), Form e) -> Form e
+mapOffset (ImageScaleRate xs ys) ((x, y), original) = move relPos original where
+  relPos = V2 (fromIntegral $ x * xs) (fromIntegral $ y * ys)
